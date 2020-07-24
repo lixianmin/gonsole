ec1f8dce46d97f79da126e717dffa9088b4b9f8d
lixianmin <lixianmin@live.cn>
2020-07-24 17:40:25 +0800

    使用反射创建html table

--- tools.go
+++ tools.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"github.com/lixianmin/gonsole/logger"
 	"github.com/lixianmin/got/mathx"
+	"github.com/lixianmin/got/timex"
 	"io"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
+	"strings"
+	"time"
 )
 
 /********************************************************************
@@ -16,6 +20,84 @@ author:     lixianmin
 
 Copyright (C) - All Rights Reserved
 *********************************************************************/
+
+func ToHtmlTable(list interface{}) string {
+	var listValue = reflect.ValueOf(list)
+	if listValue.Kind() != reflect.Slice {
+		logger.Error("list should be slice")
+		return ""
+	}
+
+	var count = listValue.Len()
+	if count == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.Grow(256)
+	sb.WriteString("<table>")
+
+	var numField = writeTableHead(&sb, listValue.Index(0))
+	for i := 0; i < count; i++ {
+		var item = listValue.Index(i)
+		sb.WriteString("<tr>")
+
+		// 写入序号
+		_, _ = fmt.Fprintf(&sb, "<td>%d</td>", i+1)
+
+		for j := 0; j < numField; j++ {
+			var field = item.Field(j)
+			sb.WriteString("<td>")
+
+			var kind = field.Kind()
+			switch kind {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				_, _ = fmt.Fprintf(&sb, "%d", field.Int())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				_, _ = fmt.Fprintf(&sb, "%d", field.Uint())
+			case reflect.Float32, reflect.Float64:
+				_, _ = fmt.Fprintf(&sb, "%.3f", field.Float())
+			case reflect.String:
+				var v = field.String()
+				sb.WriteString(v)
+			case reflect.Struct:
+				var t, ok = field.Interface().(time.Time)
+				if ok {
+					var v = timex.FormatTime(t)
+					sb.WriteString(v)
+					break
+				}
+				fallthrough
+			default:
+				logger.Error("invalid type=%+v", field.Interface())
+			}
+			sb.WriteString("</td>")
+		}
+		sb.WriteString("</tr>")
+	}
+
+	sb.WriteString("</table>")
+	var html = sb.String()
+	return html
+}
+
+func writeTableHead(sb *strings.Builder, item reflect.Value) int {
+	// 第一列用于显示序号
+	sb.WriteString("<tr> <th></th>")
+
+	// 每一列的名字
+	var itemType = item.Type()
+	var numField = itemType.NumField()
+	for i := 0; i < numField; i++ {
+		var field = itemType.Field(i)
+		sb.WriteString("<th>")
+		sb.WriteString(field.Name)
+		sb.WriteString("<th/>")
+	}
+
+	sb.WriteString("</tr>")
+	return numField
+}
 
 // https://delveshal.github.io/2018/05/17/golang-%E5%AE%9E%E7%8E%B0%E6%96%87%E4%BB%B6%E6%96%AD%E7%82%B9%E7%BB%AD%E4%BC%A0-demo/
 func RequestFileByRange(fullPath string, writer http.ResponseWriter, request *http.Request) {
---
+++ tools_test.go
@@ -0,0 +1,38 @@
+package gonsole
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+/********************************************************************
+created:    2020-07-24
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+type TestHtml struct {
+	Height     float64
+	Age        int
+	Name       string
+	CreateTime time.Time
+}
+
+func TestToHtmlTable(t *testing.T) {
+	var list = []TestHtml{{
+		Height:     10.29,
+		Age:        98,
+		Name:       "pet",
+		CreateTime: time.Now(),
+	}, {
+		Height:     5.6,
+		Age:        85,
+		Name:       "滴滴",
+		CreateTime: time.Now(),
+	}}
+
+	var html = ToHtmlTable(list)
+	fmt.Println(html)
+}
