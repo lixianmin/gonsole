be5105e90fa5bd799ed13593a32593311d25116a
lixianmin <lixianmin@live.cn>
2020-09-03 19:06:21 +0800

    refactor

--- consts.go
+++ consts.go
@@ -8,13 +8,8 @@ Copyright (C) - All Rights Reserved
 *********************************************************************/
 
 const (
-	OK            = 0   // 正确返回
-	InternalError = 500 // 内部错误
+	//OK            = 0   // 正确返回
+	//InternalError = 500 // 内部错误
 
-	UnknownError       = 601 // 未知错误
-	UnknownCommand     = 602 // 未知的debug命令
-	InvalidTopic       = 603 // 非法topic
-	InvalidOperation   = 604 // 非法操作
-
-	websocketName = "ws_console"
+	websocketName = "gonsole"
 )
