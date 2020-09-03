3b5b4920fce3e1fc095100693e004c6fdc3ce107
lixianmin <lixianmin@live.cn>
2020-09-03 14:05:06 +0800

    强制在vendor中包含进res/js目录中的js文件

---
+++ init.go
@@ -0,0 +1,14 @@
+package gonsole
+
+import "github.com/lixianmin/gonsole/res/js"
+
+/********************************************************************
+created:    2020-09-03
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+func init() {
+	js.ForceIncludeJsFiles()
+}
---
+++ res/js/init.go
@@ -0,0 +1,13 @@
+package js
+
+/********************************************************************
+created:    2020-09-03
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+// 这个文件的目的是强制在vendor中包含进res/js目录中的js文件，否则console.html中include *.js下载不到
+func ForceIncludeJsFiles() {
+
+}
