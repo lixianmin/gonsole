367ed37790f80b046e4c97575bf24fc42f3463bb
lixianmin01 <lixianmin01@baidu.com>
2021-06-02 13:12:06 +0800

    启用SetMutexProfileFraction()，激活mutex分析

--- server.go
+++ server.go
@@ -203,20 +203,6 @@ func (server *Server) enablePProf(mux IServeMux) {
 		}
 	}
 
-	// block需要特殊处理
-	var blockHandler = func(w http.ResponseWriter, r *http.Request) {
-		rate, _ := strconv.Atoi(r.FormValue("rate"))
-		if rate == 0 {
-			rate = 10000
-		}
-
-		// 这个需要先设值，再解绑
-		runtime.SetBlockProfileRate(rate)
-		defer runtime.SetBlockProfileRate(0)
-
-		pprof.Handler("block").ServeHTTP(w, r)
-	}
-
 	const root = "" // 这个不能随便改，改完了能打开页面，但看不到数据；去看看pprof.Index()的实现，里面路径写死了
 	mux.HandleFunc(root+"/debug/pprof/", handler(pprof.Index))
 	mux.HandleFunc(root+"/debug/pprof/cmdline", handler(pprof.Cmdline))
@@ -226,6 +212,32 @@ func (server *Server) enablePProf(mux IServeMux) {
 	mux.HandleFunc(root+"/debug/pprof/block", handler(blockHandler))
 	mux.HandleFunc(root+"/debug/pprof/goroutine", handler(pprof.Handler("goroutine").ServeHTTP))
 	mux.HandleFunc(root+"/debug/pprof/heap", handler(pprof.Handler("heap").ServeHTTP))
-	mux.HandleFunc(root+"/debug/pprof/mutex", handler(pprof.Handler("mutex").ServeHTTP))
+	mux.HandleFunc(root+"/debug/pprof/mutex", handler(mutexHandler))
 	mux.HandleFunc(root+"/debug/pprof/threadcreate", handler(pprof.Handler("threadcreate").ServeHTTP))
+}
+
+func blockHandler(w http.ResponseWriter, r *http.Request) {
+	rate, _ := strconv.Atoi(r.FormValue("rate"))
+	if rate == 0 {
+		rate = 10000
+	}
+
+	// 这个需要先设值，再解绑
+	runtime.SetBlockProfileRate(rate)
+	defer runtime.SetBlockProfileRate(0)
+
+	pprof.Handler("block").ServeHTTP(w, r)
+}
+
+func mutexHandler(w http.ResponseWriter, r *http.Request) {
+	rate, _ := strconv.Atoi(r.FormValue("rate"))
+	if rate == 0 {
+		rate = 10000
+	}
+
+	// 这个需要先设值，再解绑
+	runtime.SetMutexProfileFraction(rate)
+	defer runtime.SetMutexProfileFraction(0)
+
+	pprof.Handler("mutex").ServeHTTP(w, r)
 }
