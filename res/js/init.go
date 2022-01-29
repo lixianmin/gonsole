c93922be77c83483ea48a89ae65fd9a0873ce84c
lixianmin01 <lixianmin01@xmli.local>
2022-01-29 15:06:27 +0800

    refactor

--- res/js/init.go
+++ res/js/init.go
@@ -7,7 +7,7 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
-// 这个文件的目的是强制在vendor中包含进res/js目录中的js文件，否则console.html中include *.js下载不到
+// ForceIncludeJsFiles 这个文件的目的是强制在vendor中包含进res/js目录中的js文件，否则console.html中include *.js下载不到
 func ForceIncludeJsFiles() {
 
 }
