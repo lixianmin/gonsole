9516bbaab9efb1d4646755b86321b2634fe32ce0
lixianmin01 <lixianmin01@xmli.local>
2022-02-18 13:26:55 +0800

    修改build时使用的vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'console.html'),
+                nested: resolve(__dirname, 'index.html')
+            }
+        }
+    }
 })
