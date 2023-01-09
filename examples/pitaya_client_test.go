554559ffea53e4e556c98974b93bfd61df7ec75e
lixianmin <lixianmin@live.cn>
2023-01-09 17:18:19 +0800

    补测试日志文件

--- examples/pitaya_client_test.go
+++ examples/pitaya_client_test.go
@@ -18,7 +18,24 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
+func initLogo() {
+	var theLogger = logo.GetLogger().(*logo.Logger)
+	// 开启异步写标记，提高日志输出性能
+	theLogger.AddFlag(logo.LogAsyncWrite)
+
+	// 调整theLogger的filterLevel
+	var level = logo.LevelDebug
+	theLogger.SetFilterLevel(level)
+
+	// 文件日志
+	const flag = logo.FlagDate | logo.FlagTime | logo.FlagShortFile | logo.FlagLevel
+	var rollingFile = logo.NewRollingFileHook(logo.RollingFileHookArgs{Flag: flag, FilterLevel: level})
+	theLogger.AddHook(rollingFile)
+}
+
 func TestPitayaClient(t *testing.T) {
+	initLogo()
+
 	var tcpPort = 6666
 	var tcpAddress = fmt.Sprintf("127.0.0.1:%d", tcpPort)
 	var acceptor = epoll.NewTcpAcceptor(tcpAddress)
@@ -26,9 +43,8 @@ func TestPitayaClient(t *testing.T) {
 		road.WithSessionRateLimitBySecond(1000),
 	)
 
-	var count = 3000
+	var count = 300
 	var wg sync.WaitGroup
-	wg.Add(count)
 
 	app.OnHandShaken(func(session road.Session) {
 		type Challenge struct {
@@ -39,13 +55,16 @@ func TestPitayaClient(t *testing.T) {
 			if err := session.Push("player.challenge", Challenge{
 				Nonce: i,
 			}); err != nil {
-				logo.JsonE("err", err)
+				logo.JsonE("session", session.Id(), "err", err)
 			}
 		}
 	})
 
-	if err := pitayaConnect(fmt.Sprintf("127.0.0.1:%d", tcpPort), &wg); err != nil {
-		logo.JsonE("err", err)
+	for i := 0; i < 100; i++ {
+		wg.Add(count)
+		if err := pitayaConnect(fmt.Sprintf("127.0.0.1:%d", tcpPort), &wg); err != nil {
+			logo.JsonE("err", err)
+		}
 	}
 
 	//select {}
