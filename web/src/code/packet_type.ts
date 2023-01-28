87c85762bedb78bb87af209c95685a0408ff9d37
lixianmin <lixianmin@live.cn>
2023-01-28 09:29:26 +0800

    使用对象字面量代替enum

--- web/src/code/packet_type.ts
+++ web/src/code/packet_type.ts
@@ -5,10 +5,11 @@
  Copyright (C) - All Rights Reserved
  *********************************************************************/
 
-export enum PacketType {
-    Handshake = 1,
-    HandshakeAck = 2,
-    Heartbeat = 3,
-    Data = 4,
-    Kick = 5,
+// 其实就是typescript中的enum编译之后的样子, 所以没有必要用enum
+export const PacketType = {
+    Handshake: 1,
+    HandshakeAck: 2,
+    Heartbeat: 3,
+    Data: 4,
+    Kick: 5,
 }
