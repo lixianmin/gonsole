7d7b48a573df3a2e057436d3dd9f3b8a02c4e9c3
lixianmin <lixianmin@live.cn>
2023-02-01 20:32:53 +0800

    修改heatbeat.setTimeout的jsdoc的函数签名, 否则会编译不过

--- web/src/code/heartbeat.js
+++ web/src/code/heartbeat.js
@@ -7,14 +7,13 @@
  Copyright (C) - All Rights Reserved
  *********************************************************************/
 
-// /**
-//  * @typedef {Object} Heartbeat
-//  * @property {function(callback : function)} setTimeout
-//  * @property {function()} clearTimeout
-//  * @property {number} interval
-//  * @returns {Heartbeat}
-//  */
-
+/**
+ * @typedef {Object} Heartbeat
+ * @property {function(callback)} setTimeout --函数签名如果写成{function(callback : function)}, 则会被认为是2个参数, 然后导致编译不过
+ * @property {function()} clearTimeout
+ * @property {number} interval
+ * @returns {Heartbeat}
+ */
 export function createHeartbeat() {
     let _interval = 10 * 1000 // 心跳间隔, 单位(ms)
     let _timeoutId = 0
