b4fb6df7259d1fe63a727c0c77fe9972406051bc
lixianmin <lixianmin@gmail.com>
2023-03-21 20:02:19 +0800

    npm run dev支持https

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,15 @@
 import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import {resolve} from 'path'
+import path, {resolve} from 'path'
 
 export default defineConfig({
     plugins: [solidPlugin()],
     server: {
         port: 3000,
+        https: {
+            key: path.resolve(__dirname, '../res/ssl/localhost.key'),
+            cert: path.resolve(__dirname, '../res/ssl/localhost.crt'),
+        }
     },
     css: {  // 阻止vite在编译时把css选择器的名字改掉
         modules: false
