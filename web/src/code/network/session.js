feac81222ccb17729755a6219c11a4a5559aada0
lixianmin <lixianmin@gmail.com>
2023-06-09 22:09:05 +0800

    加入connect方法

--- web/src/code/network/session.js
+++ web/src/code/network/session.js
@@ -11,23 +11,26 @@ import {newOctetsReader} from "@src/code/iox/octets_reader";
 import {decode, encode} from "@src/code/serde/tools";
 import {PacketKind} from "@src/code/serde/consts";
 import {newJsonSerde} from "@src/code/serde/json_serde";
-import {newHeartbeat} from "@src/code/network/heartbeat";
 import {newOctetsWriter} from "@src/code/iox/octets_writer";
 
-export function newSession(url) {
-    const socket = new WebSocket(url)
-    socket.binaryType = 'arraybuffer'
-    socket.onopen = onopen
-    socket.onmessage = onmessage
-    socket.onerror = onerror
-    socket.onclose = onclose
+export function newSession() {
+    const _serde = newJsonSerde()
+    const _reader = newOctetsReader(newOctetsStream())
+    const _writer = newOctetsWriter(newOctetsStream())
 
-    const serde = newJsonSerde()
-    const reader = newOctetsReader(newOctetsStream())
-    const writer = newOctetsWriter(newOctetsStream())
+    let _onConnected = undefined
+    let _socket = undefined
+    let _kindRoutes = new Map()
 
-    let handshake = undefined
-    const heartbeat = newHeartbeat()
+    function connect(url, onConnected) {
+        _onConnected = onConnected
+        _socket = new WebSocket(url)
+        _socket.binaryType = 'arraybuffer'
+        _socket.onopen = onopen
+        _socket.onmessage = onmessage
+        _socket.onerror = onerror
+        _socket.onclose = onclose
+    }
 
     function onopen() {
 
@@ -35,12 +38,12 @@ export function newSession(url) {
 
     function onmessage(evt) {
         const data = new Uint8Array(evt.data)
-        const stream = reader.stream
+        const stream = _reader.stream
         stream.write(data, 0, data.length)
         stream.seek(0, SeekOrigin.Begin)
         // console.log(`onmessage: data.length=${data.length}, stream=`, stream)
 
-        onReceivedData(reader)
+        onReceivedData(_reader)
         stream.tidy()
     }
 
@@ -50,10 +53,6 @@ export function newSession(url) {
 
     function onclose() {
 
-    }
-
-    function send(bytes) {
-        socket.send(bytes.buffer)
     }
 
     function onReceivedData(reader) {
@@ -70,9 +69,10 @@ export function newSession(url) {
                 onReceivedHandshake(pack)
                 break
             case PacketKind.Heartbeat:
-                console.log(pack)
+                // console.log(pack)
                 break
             case PacketKind.Kick:
+                _socket.close(0, 'kicked')
                 break
             default:
                 if (pack.kind >= PacketKind.UserDefined) {
@@ -83,19 +83,19 @@ export function newSession(url) {
     }
 
     function onReceivedHandshake(pack) {
-        handshake = serde.deserialize(pack.data)
+        const handshake = _serde.deserialize(pack.data)
         buildKindRoutes()
         startHeartbeat()
 
+        if (_onConnected) {
+            _onConnected(handshake.nonce)
+        }
+
         function buildKindRoutes() {
-            const kind_routes = new Map()
-
+            _kindRoutes.clear()
             for (let [route, kind] of Object.entries(handshake.route_kinds)) {
-                kind_routes.set(kind, route)
+                _kindRoutes.set(kind, route)
             }
-
-            handshake.kind_routes = kind_routes
-            delete handshake.route_kinds
         }
 
         function startHeartbeat() {
@@ -106,22 +106,21 @@ export function newSession(url) {
             }, interval)
         }
 
-
-        heartbeat.interval = handshake.heartbeat * 1000 // unit: ms
-        console.log(handshake)
+        console.log('handshake', handshake)
     }
 
     function sendPacket(pack) {
-        const stream = writer.stream
+        const stream = _writer.stream
         stream.reset()
-        encode(writer, pack)
+        encode(_writer, pack)
 
         stream.seek(0, SeekOrigin.Begin)
         const bytes = stream.bytes
-        socket.send(bytes.buffer)
+        _socket.send(bytes.buffer)
     }
 
     return {
+        connect: connect,
         sendPacket: sendPacket,
     }
 }
