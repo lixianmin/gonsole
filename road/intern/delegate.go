efce6f6293e85f567984580f0b0c34ad44c048dd
lixianmin <lixianmin@live.cn>
2024-10-23 13:56:20 +0800

    注释掉delegate

--- road/intern/delegate.go
+++ road/intern/delegate.go
@@ -1,47 +1,47 @@
 package intern
 
-import (
-	"github.com/lixianmin/logo"
-	"slices"
-	"sync"
-)
+// import (
+// 	"github.com/lixianmin/logo"
+// 	"slices"
+// 	"sync"
+// )
 
-/********************************************************************
-created:    2020-09-04
-author:     lixianmin
+// /********************************************************************
+// created:    2020-09-04
+// author:     lixianmin
 
-Copyright (C) - All Rights Reserved
-*********************************************************************/
+// Copyright (C) - All Rights Reserved
+// *********************************************************************/
 
-type delegate struct {
-	handlers []func()
-	lock     sync.Mutex
-}
+// type delegate struct {
+// 	handlers []func()
+// 	lock     sync.Mutex
+// }
 
-func (my *delegate) Add(handler func()) {
-	if handler != nil {
-		my.lock.Lock()
-		my.handlers = append(my.handlers, handler)
-		my.lock.Unlock()
-	}
-}
+// func (my *delegate) Add(handler func()) {
+// 	if handler != nil {
+// 		my.lock.Lock()
+// 		my.handlers = append(my.handlers, handler)
+// 		my.lock.Unlock()
+// 	}
+// }
 
-func (my *delegate) Invoke() {
-	// 单独clone一份出来，因为callback的方法体调用了哪些内容未知，防止循环调用导致死循环
-	//
-	// 2022-09-03 对循环调用导致死循环这事, clone一份出来真的能解决问题嘛? 存疑!
-	// 通常, 我们copy一份出来是为了减少临界区占用的时间
-	my.lock.Lock()
-	var cloned = slices.Clone(my.handlers)
-	my.lock.Unlock()
+// func (my *delegate) Invoke() {
+// 	// 单独clone一份出来，因为callback的方法体调用了哪些内容未知，防止循环调用导致死循环
+// 	//
+// 	// 2022-09-03 对循环调用导致死循环这事, clone一份出来真的能解决问题嘛? 存疑!
+// 	// 通常, 我们copy一份出来是为了减少临界区占用的时间
+// 	my.lock.Lock()
+// 	var cloned = slices.Clone(my.handlers)
+// 	my.lock.Unlock()
 
-	defer func() {
-		if r := recover(); r != nil {
-			logo.Info("[Invoke()] panic: r=%v", r)
-		}
-	}()
+// 	defer func() {
+// 		if r := recover(); r != nil {
+// 			logo.Info("[Invoke()] panic: r=%v", r)
+// 		}
+// 	}()
 
-	for _, handler := range cloned {
-		handler()
-	}
-}
+// 	for _, handler := range cloned {
+// 		handler()
+// 	}
+// }
